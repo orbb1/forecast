e351183e5e4976a2a327956c4b2b44642b8b1e6b
orbb <orbb@localhost.localdomain>
2017-02-15 18:31:10 +0100

    home page initial commit

--- app/modules/routing-module/routing.module.js
+++ app/modules/routing-module/routing.module.js
@@ -5,13 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var core_1 = require('@angular/core');
-var router_1 = require('@angular/router');
-var login_page_component_1 = require('../../../app/pages/login-page/login-page.component');
-var home_page_component_1 = require('../../../app/pages/home-page/home-page.component');
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var login_page_component_1 = require("../../../app/pages/login-page/login-page.component");
+var home_page_component_1 = require("../../../app/pages/home-page/home-page.component");
 var appRoutes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: login_page_component_1.LoginPageComponent },
@@ -20,14 +17,13 @@ var appRoutes = [
 var RoutingModule = (function () {
     function RoutingModule() {
     }
-    RoutingModule = __decorate([
-        core_1.NgModule({
-            imports: [router_1.RouterModule.forRoot(appRoutes)],
-            exports: [router_1.RouterModule]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], RoutingModule);
     return RoutingModule;
 }());
+RoutingModule = __decorate([
+    core_1.NgModule({
+        imports: [router_1.RouterModule.forRoot(appRoutes)],
+        exports: [router_1.RouterModule]
+    })
+], RoutingModule);
 exports.RoutingModule = RoutingModule;
 //# sourceMappingURL=routing.module.js.map
